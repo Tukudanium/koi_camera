275cf2bf659196c3f47196679bc0c1f9d3e3e587
sagawa masanori <sagawa.masanori@fic.co.jp>
2022-11-19 16:54:19 +0900

    更新

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from base64 import b64encode, b64decode
 import cv2
-from werkzeug.utils import secure_filename
 from flask import *
 from flask_bootstrap import Bootstrap
+from PIL import Image
+import numpy as np
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./images')
 bootstrap = Bootstrap(app)
 
 image = '' #差し替え後の画像の保存位置
@@ -45,6 +46,30 @@ def video_feed():
     stop_run = False #カメラを動かす
     # コマ撮りの画像を常に更新し続ける
     return Response(getFrames(), mimetype='multipart/x-mixed-replace; boundary=boundary')
+
+#画像インポートページのURL
+@app.route('/upload',methods=['POST','GET'])
+def upload_page():
+    if request.method == 'GET':
+        global stop_run
+        stop_run = True #カメラを止める
+        while stopped == False:
+            stopped #カメラの処理が終了するまで待つ
+
+        return render_template("upload.html")
+    if request.method == 'POST':
+        if 'input_file' not in request.files:
+            return redirect(request.url)
+        # データの取り出し
+        file = request.files['input_file']
+        # ファイル名がなかった時の処理
+        if file.filename == '':
+            return redirect(request.url)
+        data = request.files['input_file']
+        img = Image.open(data)
+        result = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
+        face_to_image(result)
+        return redirect(url_for('save_page'))
 
 
 #################
@@ -147,6 +172,44 @@ def getFrames():
     cap.release()
     stopped = True
     return
+
+def face_to_image(image_item):
+    frame = image_item
+    aveSize = 0
+    count = 0
+    image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30)) #顔の範囲を取得
+    # 顔を検出した場合
+    if len(facerect) > 0:
+        for rect in facerect:
+            count += 1
+            if count < 4:
+                aveSize += (rect[2] + rect[3]) / 2
+                break
+            elif count == 4:
+                aveSize += (rect[2] + rect[3]) / 2
+                aveSize /= 5
+            else:
+                aveSize = aveSize * 0.8 + rect[2] * 0.1 + rect[3] * 0.1
+            thresh = aveSize * 0.95  # 移動平均の95%以上を閾値
+            if rect[2] < thresh or rect[3] < thresh:
+                break
+            # 検出した顔を囲む矩形の作成
+            #color = (255, 100, 100)
+            #cv2.rectangle(frame, tuple(rect[0:2]), tuple( rect[0:2]+rect[2:4]), color, thickness=2)
+            faceImg = cv2.resize(
+                faceImg, ((int)(rect[2] * 1.3), (int)(rect[3] * 1.3)), cv2.IMREAD_UNCHANGED)
+            rect[0] -= rect[2] * 0.15  # x_offset
+            rect[1] -= rect[3] * 0.15  # y_offset
+            # 顔の部分に画像挿入
+            #FIXME 顔を大きく近づけたり、画面端に顔があるとエラー
+            #      おそらく表示している画面の範囲を顔の認識部分が超えてしまったことによる
+            #      強制終了する訳では無いが、エラー中は重ねる画像は出ない
+            frame[rect[1]:rect[1] + faceImg.shape[0],rect[0]:rect[0] + faceImg.shape[1]] = frame[rect[1]:rect[1] + faceImg.shape[0], rect[0]:rect[0] + faceImg.shape[1]] * (1 - faceImg[:, :, 3:] / 255) + faceImg[:, :, :3] * (faceImg[:, :, 3:] / 255)
+    global image #現在の画像を更新
+    ret, image = cv2.imencode(".jpg", frame)
+    return
+
 
 #ブラウザでローカルホストを自動で開くだけ
 import webbrowser
